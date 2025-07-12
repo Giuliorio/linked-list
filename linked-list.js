3065309d29ee99d384a790ab1ef5a09d62b1e19d
Giulio Riondino <riondino.giulio@gmail.com>
2025-07-11 17:32:48 -0700

    Add LinkedList class with head and stubbed tail getter

--- linked-list.js
+++ linked-list.js
@@ -12,3 +12,25 @@ class Node {
     this.nextNode = null;
   }
 }
+
+/**
+ * Represents a linked list.
+ */
+
+class LinkedList {
+  #head = null;
+
+  /**
+   * @returns {Node|null} The head node or null if the list is empty.
+   */
+  get head() {
+    return this.#head;
+  }
+
+  /**
+   * @returns {Node|null} The tail or null if the list is empty.
+   */
+  get tail() {
+    return null;
+  }
+}
