252cf221b3ddc8011f7b9701d794ecd105e9df90
Giulio Riondino <riondino.giulio@gmail.com>
2025-07-11 17:47:29 -0700

    Add append and prepend methods to LinkedList

--- linked-list.js
+++ linked-list.js
@@ -41,6 +41,34 @@ class LinkedList {
   }
 
   /**
+   * Adds a new node with the given value at the end of the list.
+   * @param {*} value - The Value to append.
+   */
+  append(value) {
+    const node = new Node(value);
+
+    if (!this.#head) {
+      this.#head = node;
+    } else {
+      const tail = this.tail;
+
+      tail.nextNode = node;
+    }
+  }
+
+  /**
+   * Adds a new node with the given value at the end of the list
+   * @param {*} value The value to prepend.
+   */
+  prepend(value) {
+    const node = new Node(value);
+
+    node.nextNode = this.#head;
+
+    this.#head = node;
+  }
+
+  /**
    * Traverses the list and calls a callback for each node.
    * @param {(node: Node, index: number) => void} callback
    */
